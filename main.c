20e8ebb3d90429138b7dec63e82660c0d1866189
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-09 10:05:49 +0100

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
+//Cette fichier main.c lit le fichier CSV, extrait les données et les insère dans un arbre AVL
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-Ouverture de fichier
+// Structure pour les données électriques
+typedef struct electricite {
+    int identifiant; //identifiant de la centrale éléctrique
+    int hvb_station;
+    int hva_station;
+    int lv_station;
+    long capacite;
+}Electricite;
 
+// Fonction pour lire un fichier CSV et traiter les données
+void fichier_csv(const char* fichier) {
+    FILE* fichier=fopen("nomdefichier.txt", "r");
+    if (!file) {
+        perror("Erreur lors de l'ouverture du fichier");
+        exit(EXIT_FAILURE);
+    }
 int main(){
   FILE*fichier=fopen("fichier;txt","r");
   
+  const char* fichier_csv = "c-wire_000.csv";
+
+    printf("Lecture et traitement du fichier CSV : %s\n", fichier_csv);
+    traiter_fichier_csv(fichier_csv);
+
+    return 0;
+}
