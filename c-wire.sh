f445960c5c43b63d0df1125fd866b885c7eb2cce
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-11 12:18:28 +0100

    c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
- 
-if [ $# -lt 1 ]
-then
-	echo "probleme: nombre d'argument"
-	exit()
+
+# Check if the minimum number of arguments is provided
+if [ $# -lt 1 ]; then
+    echo "Problème: nombre d'argument insuffisant"
+    exit 1
 fi
 
-if [ $2="hvb" ]
-then
-	station=1
-	
-elif [ $2="hva" ]
-then 
-	station=2
-	
-elif [ $2="lv" ]
-then
-	station=3
+# Initialize variables
+station=0
+conso=0
 
-elif [ $2="comp" ]
-then
-	conso=1
-
-elif [ $2="indiv" ]
-then
-	conso=2
-	
-elif [ $2="all" ]
-then
-	conso=3
-
-else	
-	echo "probleme: mauvais argument"		
-
+# Check the value of $2
+if [ "$2" = "hvb" ]; then
+    station=1
+elif [ "$2" = "hva" ]; then
+    station=2
+elif [ "$2" = "lv" ]; then
+    station=3
+elif [ "$2" = "comp" ]; then
+    conso=1
+elif [ "$2" = "indiv" ]; then
+    conso=2
+elif [ "$2" = "all" ]; then
+    conso=3
+else
+    echo "Problème: mauvais argument"
+    exit 1
 fi
+
+# Example output to debug the result
+echo "Station: $station, Conso: $conso"
