c62fd6ea0c3151c2b3d2cb130a17c826ca10b388
Sira-la-Staaar <siradiakite792@gmail.com>
2024-12-11 11:37:27 +0100

    c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+ 
+if [ $# -lt 1 ]
+then
+	echo "probleme: nombre d'argument"
+	exit()
+fi
 
-#Inclusion de fonctions
+if [ $2="hvb" ]
+then
+	station=1
+	
+elif [ $2="hva" ]
+then 
+	station=2
+	
+elif [ $2="lv" ]
+then
+	station=3
 
+elif [ $2="comp" ]
+then
+	conso=1
+
+elif [ $2="indiv" ]
+then
+	conso=2
+	
+elif [ $2="all" ]
+then
+	conso=3
+
+else	
+	echo "probleme: mauvais argument"		
+
+fi
