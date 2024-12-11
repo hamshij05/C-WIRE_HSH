9e2d699eafe571331ec7f8bbe5b17a2970b7dbb2
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-11 11:06:10 +0100

    biblio.h

--- biblio.h
+++ biblio.h
@@ -22,5 +22,16 @@ typedef struct electricite{
 
 
 Arbre *creationarbre(int a); //Création de noeud pour l'arbre AVL
+Arbre *equilibrageAVL(Arbre*a); //Insertion de la fonction equilibre si nécessaire
+Arbre *insertionAVL(Arbre *a, int e, int *h); //Insertion de l'arbre AVL
+Arbre *creationAVLfromCSV(FILE *fichier, int e, int *h); //Création de l'arbre AVL à partir des données de fichier CSV
+Arbre *rotationGauche(Arbre *a); //Rotation gauche pour réequilibrer l'arbre
+Arbre *rotationDroite(Arbre *a); //Rotation droite pour réequilibrer l'arbre
+Arbre *doubleRotationGauche(Arbre *a); //Double Rotation Gauche
+Arbre *doubleRotationDroite(Arbre *a); //Double Rotation Droite
+int hauteur(Arbre *a); //Calcul de l'hauteur de l'arbre AVL
+
+
+
 
 
