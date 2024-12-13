c082f65862ecee130eaf8867894b6e482377fa7a
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-13 23:52:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,13 +14,16 @@ typedef struct electricite {
 }Electricite;
 
 // Fonction pour lire un fichier CSV et traiter les données
-void fichier_csv(const char* fichier) {
+void fichier_csv(const char* fichier,insertionAVL,) {
     FILE* fichier=fopen("c-wire_v00.csv", "r");
     if (fichier==NULL) {
         printf("Erreur lors de l'ouverture du fichier");
         exit(EXIT_FAILURE);
     }
+FILE*fichier=fopen("c-wire.csv","r")
 
+int parseCSV(const char *filename, AVLNode **root) {
+   
 int main(){
   FILE*fichier=fopen("c-wire.csv","r");
   
@@ -32,7 +35,29 @@ int main(){
     return 0;
 }
 
+int main(int argc, char *argv[])
+{
+    int RouteID;
+    float distance;
+    char header[256];
+    int h = 0;
 
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Error while trying to open csv file.\n");
+        exit(1);
+    }
+
+    // Creates the AVL root with first line of csv
+    fgets(header, sizeof(header), file); // Skips header line
+
+    if (fscanf(file, "%d;%*d;%*[^;];%*[^;];%f;%*[^\n]", &RouteID, &distance) != 2)
+    {
+        printf("Error while gettings variables of csv.\n");
+        exit(1);
+    }
+    pAVL root = CreateAVL(RouteID, distance);
 
 
 
