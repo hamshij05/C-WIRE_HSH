beb58e0004e8a9e2a5b7fcd4e391bbd4fd0e30c7
Sira-la-Staaar <siradiakite792@gmail.com>
2024-12-21 19:02:00 +0100

    c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 
 # Fonction pour arrête le chronomètre et afficher la durée
-fin_chrono() {
+#temps_1=$(date +%s.%N)
+fin_chrono() { #fait à l'aide de Chat GPT
+    # Vérifier si $1 est un nombre valide (entier ou flottant)
+    if ! [[ "$1" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        echo "Erreur : L'argument de temps ($1) n'est pas un nombre valide."
+        exit 1
+    fi
+
+    # Obtenir le temps actuel
     temps_2=$(date +%s.%N)
-    temps=$(echo "$temps_2 - $1" | bc)
-    echo "Durée utile du traitement : ${temps} sec"
+
+    # Vérifier que temps_2 est un nombre valide
+    if ! [[ "$temps_2" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        echo "Erreur : Le calcul du temps a échoué, temps_2 n'est pas valide."
+        exit 1
+    fi
+
+    # Effectuer le calcul du temps avec une précision de 2 décimales
+    temps=$(echo "scale=2; $temps_2 - $1" | bc)
+
+    # Si le résultat est trop petit (par exemple moins de 0.01 sec), on peut l'afficher avec une précision contrôlée
+    if [ $(echo "$temps < 0.01" | bc) -eq 1 ]; then
+        echo "Durée utile du traitement : 0.00 sec"
+    else
+        echo "Durée utile du traitement : $temps sec"
+    fi
 }
+
 
 aide() { 
-    echo "Usage: bash <chemin(c-wire.sh)> <station> <consommateur> (<id centrale>)"
-    echo "station : hvb, hva, lv"
-    echo "type : comp, indiv, all"
-    echo "chemin : nom du fichier ou autre"
-    echo "PS: une hva et une hvb ne peut avoir comme consommateur qu'une entreprise (comp)"  
+	echo "assurez vous d'abord que le fichier à traiter est le premier argument et qu'il est dans le même dossier que celui-ci"	
+	echo "Usage: bash <chemin(c-wire.sh)> <station> <consommateur> (<id centrale>)"
+	echo "station : hvb, hva, lv"
+	echo "type : comp, indiv, all"
+	echo "chemin : nom du fichier ou autre"
+	echo "PS: une hva et une hvb ne peut avoir comme consommateur qu'une entreprise (comp)"  
+	exit 1
+}
+
+fichier=$1
+if [ ! -f "$fichier" ]; then
+    echo "Erreur: le fichier $fichier n'existe pas"
+    fin_chrono $temps_1
     exit 1
-}
-
-nom_fichier() {
-nom_fichier="${1}_${2}.csv"
-}
-
+fi
 
 if [ $# -lt 1 ]; 
 	then
@@ -45,6 +71,7 @@ elif [ "$2" = "lv" ];
 else
 	echo "Problème: mauvaise station argument"
  	echo "Durée utile du traitement : 0.0 sec"
+ 	echo "Station: $station, Conso: $conso"
 	aide
 	exit 1
 fi
@@ -90,46 +117,7 @@ plant="$4"
 #echo "Station: $station, Conso: $conso"
 
 
-EXECUTABLE="main"
-
-# Vérification de la présence du Makefile
-if [ ! -f "Makefile" ]; then
-    echo "Erreur: Le fichier Makefile est introuvable."
-    exit 1
-fi
-
-# Vérification de la présence de l'exécutable
-if [ ! -f "$EXECUTABLE" ]; then
-    echo "L'exécutable $EXECUTABLE est introuvable. Lancement de la compilation"
-    
-    # lancer la compilation avec le Makefile
-    make "$EXECUTABLE"
-    
-    # Vérification si make a réussi
-    if [ $? -ne 0 ]; then
-        echo "Erreur: La compilation avec make a échoué."
-        exit 1
-    fi
-    
-    echo "Compilation réussie. Exécutable $EXECUTABLE généré."
-fi
-
-# Vérification que l'exécutable a bien été créé après la compilation
-if [ ! -f "$EXECUTABLE" ]; then
-    echo "Erreur: L'exécutable $EXECUTABLE est introuvable même après compilation."
-    exit 1
-fi
-
-# Exécuter l'exécutable avec les arguments donnés
-echo "Exécution de l'exécutable $EXECUTABLE avec les arguments : $@"
-./"$EXECUTABLE" "$@"
-
-# Vérification de la réussite de l'exécution
-if [ $? -ne 0 ]; then
-    echo "Erreur : L'exécution du programme a échoué."
-    exit 1
-fi
-
+###################################################################################################
 
 #dossier tmp
 if [  -d "tmp" ];
@@ -167,7 +155,6 @@ else
 		exit 1
 	fi
 fi
-	
 temps_1=$(date +%s.%N)
 
 name_file1="hvb_comp.csv"
@@ -175,14 +162,67 @@ cd tmp
 touch "$name_file1"
 if [ $? -ne 0 ];
 		then 
-		echo "Erreur: impossibilité de créer le fichier demandé $name_file"
+		echo "Erreur: impossibilité de créer le fichier demandé $name_file1"
 		fin_chrono $temps_1
   		exit 1
 fi
-head -n 1 c-wire_v00.csv > $name_file1
-grep -E "[1-9];[1-9]+;-;-;[1-9]+;-;-;[1-9]+" c-wire_v00.csv | cut -d":" -f1 > $name_file1
+head -n 1 ../$fichier | cut -d';' -f2,7,8 > $name_file1 
+grep -E "[1-9];[1-9]+;-;-;-;-;[1-9]+;-" ../$fichier | cut -d';' -f2,7,8 | sed 's/;/:/g' >> $name_file1 
+grep -E "[1-9];[1-9]+;-;-;[1-9]+;-;-;[1-9]+" ../$fichier | cut -d';' -f2,7,8 | sed 's/;/:/g' >> $name_file1 
 #grep -E "^[1-9];[1-9]+;-;-;" c-wire_v00.csv | cut -d":" -f1 > $name_file1
 
+name_file2="hva_comp.csv"
+touch "$name_file2"
+if [ $? -ne 0 ];
+		then 
+		echo "Erreur: impossibilité de créer le fichier demandé $name_file2"
+		fin_chrono $temps_1
+  		exit 1
+fi
+head -n 1 ../$fichier | cut -d';' -f3,7,8 > $name_file2 
+grep -E "^[1-9];[1-9]+;[1-9]+;-;-;-;[1-9]+;-" ../$fichier| cut -d';' -f3,7,8 | sed 's/;/:/g' >> $name_file2  
+grep -E "^[1-9];-;[1-9]+;-;[1-9]+;-;-;[1-9]+" ../$fichier| cut -d';' -f3,7,8 | sed 's/;/:/g' >> $name_file2
 
-fin_chrono
+
+name_file3="lv_comp.csv"
+touch "$name_file3"
+if [ $? -ne 0 ];
+		then 
+		echo "Erreur: impossibilité de créer le fichier demandé $name_file3"
+		fin_chrono $temps_1
+  		exit 1
+fi 
+head -n 1 ../$fichier | cut -d';' -f4,7,8 > $name_file3
+grep -E "[1-9];-;[1-9]+;[1-9]+;-;-;[1-9]+;-" ../$fichier | cut -d";" -f4,7,8 | sed 's/;/:/g' >> $name_file3
+grep -E "[1-9];-;-;[1-9]+;[1-9]+;-;-;[1-9]+" ../$fichier | cut -d';' -f4,7,8 | sed 's/;/:/g' >> $name_file3
+
+
+name_file4="lv_indiv.csv"
+touch "$name_file4"
+if [ $? -ne 0 ];
+		then 
+		echo "Erreur: impossibilité de créer le fichier demandé $name_file4"
+		fin_chrono $temps_1
+  		exit 1
+fi
+head -n 1 ../$fichier | cut -d';' -f4,7,8 > $name_file4
+grep -E "[1-9];-;-;[1-9]+;-;[1-9]+;-;[1-9]+" ../$fichier | cut -d';' -f4,7,8 | sed 's/;/:/g' >> $name_file4
+grep -E "[1-9];-;[1-9]+;[1-9]+;-;-;[1-9]+;-" ../$fichier | cut -d";" -f4,7,8 | sed 's/;/:/g' >> $name_file4
+
+name_file5="lv_all.csv"
+touch "$name_file5"
+if [ $? -ne 0 ];
+		then 
+		echo "Erreur: impossibilité de créer le fichier demandé $name_file5"
+		fin_chrono $temps_1
+  		exit 1
+fi
+head -n 1 ../$fichier | cut -d';' -f4,7,8 >> $name_file5 
+grep -E "[1-9];-;[1-9]+;[1-9]+;-;-;[1-9]+;-" ../$fichier | cut -d";" -f4,7,8 | sed 's/;/:/g' >> $name_file5
+grep -E "[1-9];-;-;[1-9]+;[1-9]+;-;-;[1-9]+" ../$fichier | cut -d';' -f4,7,8 | sed 's/;/:/g' >> $name_file5
+grep -E "[1-9];-;-;[1-9]+;-;[1-9]+;-;[1-9]+" ../$fichier | cut -d';' -f4,7,8 | sed 's/;/:/g' >> $name_file5
+
+cd ..
+
+fin_chrono $temps_1
 	
