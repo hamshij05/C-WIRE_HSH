11575a609980b70b1fbf907148586a17b98ce933
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-21 22:33:01 +0100

    biblio.h

--- biblio.h
+++ biblio.h
@@ -22,12 +22,14 @@ typedef struct arbre{
 Arbre *creationArbre(long identifiant, long capacite, long consommation); //Création de noeud pour l'arbre AVL
 Arbre *insertionAVL(Arbre *a, long identifiant, long capacite, long consommation); //Insertion de l'arbre AVL
 long max(long x, long y); //Fonction pour obtenir la valeur maximale
-Arbre *rotationGauche(Arbre *a); //Rotation gauche pour réequilibrer l'arbre
+Arbre *rotationGauche(Arbre *pivot); //Rotation gauche pour réequilibrer l'arbre
 Arbre *rotationDroite(Arbre *a); //Rotation droite pour réequilibrer l'arbre
-Arbre *doubleRotationGauche(Arbre *a); //Double Rotation Gauche
-Arbre *doubleRotationDroite(Arbre *a); //Double Rotation Droite
-int hauteur(Arbre *a); //Calcul de l'hauteur de l'arbre AVL
+Arbre *doubleRotationGauche(Arbre* a); //Double Rotation Gauche
+Arbre *doubleRotationDroite(Arbre* a); //Double Rotation Droite
+int hauteur(Arbre* a); //Calcul de l'hauteur de l'arbre AVL
 long facteur_equilibre(Arbre* r); //Calcul du facteur d'équilibre
+void infixe(Arbre* a, FILE* fichier_resultat); //Parcours en ordre croissant (infixe = parcours en ordre) et écriture dans le fichier de sortie
+void freeAVL(Arbre* a); //Libère la mémoire de l'arbre AVL
 
 #endif //BIBLIO_H
 
