89fe5467f23a0508229a567f4f7535c498aaf815
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-21 22:25:56 +0100

    biblio.h

--- biblio.h
+++ biblio.h
@@ -1,3 +1,6 @@
+#ifndef BIBLIO_H
+#define BIBLIO_H
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -5,32 +8,28 @@
 #include <math.h>
 
 //Structure d'Arbre AVL
-//Structure d'Arbre AVL
 typedef struct arbre{
-	char identifiant[50]; //identifiant de chaque station : Powerplant,HV-A, HV-B, LV stations
+	long identifiant; //identifiant de chaque station : Powerplant,HV-A, HV-B, LV stations
 	long capacite; //capacité de la station
 	long consommation; //consommation totale
 	struct arbre *fg;
 	struct arbre *fd;
-	int equilibre; //facteur d'équilibre
+	int equilibre; //facteur d'équilibre pour l'arbre AVL
 	int hauteur;
 }Arbre;
 
 
-Arbre *creationarbre(int a); //Création de noeud pour l'arbre AVL
-Arbre *equilibrageAVL(Arbre*a); //Insertion de la fonction equilibre si nécessaire
-Arbre *insertionAVL(Arbre *a, int e, int *h); //Insertion de l'arbre AVL
-Arbre *creationAVLfromCSV(FILE *fichier, int e, int *h); //Création de l'arbre AVL à partir des données de fichier CSV
+Arbre *creationArbre(long identifiant, long capacite, long consommation); //Création de noeud pour l'arbre AVL
+Arbre *insertionAVL(Arbre *a, long identifiant, long capacite, long consommation); //Insertion de l'arbre AVL
+long max(long x, long y); //Fonction pour obtenir la valeur maximale
 Arbre *rotationGauche(Arbre *a); //Rotation gauche pour réequilibrer l'arbre
 Arbre *rotationDroite(Arbre *a); //Rotation droite pour réequilibrer l'arbre
 Arbre *doubleRotationGauche(Arbre *a); //Double Rotation Gauche
 Arbre *doubleRotationDroite(Arbre *a); //Double Rotation Droite
-int existeFilsGauche(Arbre *a); //Vérification le fils gauche existe
-int existeFilsDroite(Arbre *a); //Vérification le fils droite existe
 int hauteur(Arbre *a); //Calcul de l'hauteur de l'arbre AVL
-void miseAjour_hauteur(Arbre *a); //La mise à jour la hauteur d'un noeud après les modifications
+long facteur_equilibre(Arbre* r); //Calcul du facteur d'équilibre
 
-ligne 198
+#endif //BIBLIO_H
 
 
 
