cc593d75116b207e1de746b6b5509edf154e7474
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2024-12-22 13:33:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 //Fonction de parsing et construction de l'arbre AVL
-Arbre *parser_file(const char *str){
+long parser_file(const char *str){
     char *restant;
     //Vérifier si la chaîne est NULL ou si elle contient "-"
-    if(str == NULL || strcmp(str, "-") == 0){
+    if(strcmp(str, "-") == 0){
         return 0; 
     }
     //Convertir la chaîne en entier en base 10
@@ -20,19 +20,19 @@ Arbre *parser_file(const char *str){
 }
 
 int main(int argc, char* argv[]){
-    if(argc < 3){
-        fprintf(stderr, "Traitement : %s <input_file> <fichier_resultat>\n", argv[0];
-        return 1;
-    }
-    const char* input_file = argv[1];
-    const char* fichier_resultat = argv[2];
-    const char* identifiant = argv[3]; //Récuperation le type de station
+if(argc < 3){
+    fprintf(stderr, "Traitement : %s <input_file> <fichier_resultat>\n", argv[0];
+    return 1;
+}
+const char* input_file = argv[1];
+const char* fichier_resultat = argv[2];
+const char* identifiant = argv[3]; //Récuperation le type de station
 
-    FILE* input = fopen(input_file, "r");
-    if(!input){
-        printf("Attention : Erreur lors de l'ouverture du fichier input");
-        return 1;
-    }
+FILE* input = fopen(input_file, "r");
+if(!input){
+    printf("Attention : Erreur lors de l'ouverture du fichier input");
+    return 1;
+}
     FILE* resultat = fopen(fichier_resultat, "w");
     if(!=resultat){
         printf("Erreur lors de l'ouverture du fichier de sortie.\n");
@@ -44,19 +44,42 @@ int main(int argc, char* argv[]){
 
     //Lecture des données et insertion dans l'arbre AVL
     while (fgets(line, sizeof(ligne), input)){
-        line[strcspn(ligne, "\n")] = 0;
-        if((strncmp(ligne, "LV Station : 
+        ligne[strcspn(ligne, "\n")] = 0;
+    if ((strncmp(ligne, "LV Station:Capacity:Load", 24) == 0 && strcmp(identifiant, "LV") == 0) || (strncmp(ligne, "HV-A Station:Capacity:Load", 25) == 0 && strcmp(identifiant, "HV-A") == 0) || (strncmp(ligne, "HV-B Station:Capacity:Load", 25) == 0 && strcmp(identifiant, "HV-B") == 0)) {
+       printf(");
+           continue;
+           }
+    char* segment = strtok(ligne, ":");
+        if(!segment){
+            fprintf(stderr, "Attention : ligne ignorée : %s\n", ligne);
+            continue;
+        }
+        long identifiant = parser_file(segment);
 
+        segment = strtok(NULL, ":");
+        if(!segment){
+            fprintf("stderr, "Attention : ligne ignorée : %s\n", ligne);
+            continue;
+        }
+        long capacite = parser_file(segment);
 
-    char*         
+        segment = strtok(NULL, ":");
+        if(!token){
+            fprintf(stderr, "Attention : ligne ignorée : %s"\n", ligne);
+            continue;
+        }
+        long load = parse_long(segment);
 
- //Ecriture des résultats triés par les identifiants
-            fprintf("resultat, "Identifiant : Capacite : Consommation \n");
-            infixe(a, resultat);
-  //Libérer la mémoire et fermeture des ficheirs
-            freeAVL(
-            fclose(input);
-            fclose(resultat);
+        a = insertion(a, identifiant, capacite, load);
+    }
+        //Ecriture des résultats triés par les identifiants
+        fprintf("resultat, "Identifiant : Capacite : Consommation \n");
+        infixe(a, resultat);
+        
+        //Libérer la mémoire et fermeture des ficheirs
+        freeAVL(a);
+        fclose(input);
+        fclose(resultat);
         return 0;
 
 }
