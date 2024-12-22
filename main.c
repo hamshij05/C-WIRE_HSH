9d28a675c3e4596fd767c568ba72ecc0413b5e1a
Sira-la-Staaar <siradiakite792@gmail.com>
2024-12-22 21:52:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
-#include <biblio.h>
+#include "biblio.h"
+#include "fonction.c"
 
 int main(int argc, char* argv[]) {
     const char *input_file = argv[1];
     const char *fichier_resultat = argv[2];
 
-    // Liste des fichiers à traiter
-    const char *fichiers[] = {"tmp/hvb_comp.csv", "tmp/hva_comp.csv", "tmp/lv_comp.csv", 
-        "tmp/lv_indiv.csv", "tmp/lv_all.csv"};
+    // Liste des fichiers à traiter dans le répertoire tmp
+    const char *fichiers[] = {
+        "tmp/hvb_comp.csv", "tmp/hva_comp.csv", "tmp/lv_comp.csv", 
+        "tmp/lv_indiv.csv", "tmp/lv_all.csv"
+    };
 
     // Traiter chaque fichier
     for (int i = 0; i < 5; ++i) {
-        char nomFichierTmp[50];
+        char nomFichierTmp[50]; // Correction : ne pas utiliser const ici
         snprintf(nomFichierTmp, sizeof(nomFichierTmp), "tmp/tmp_%d.csv", i);
 
-        traiterFichier(..\fichiers[i], nomFichierTmp);
+        // Appel de la fonction traiterFichier avec le chemin complet vers le fichier source et destination
+        traiterFichier(fichiers[i], nomFichierTmp);
     }
 
     return 0;
