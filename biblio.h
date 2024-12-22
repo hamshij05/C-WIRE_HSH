83528dbd81b76cc8eed371565e51c3104c4504ab
Sira-la-Staaar <siradiakite792@gmail.com>
2024-12-22 21:54:32 +0100

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -20,20 +20,19 @@ typedef struct arbre{
 
 
 Arbre *creationArbre(long identifiant, const char* capacite, const char* consommation); //Création de noeud pour l'arbre AVL
-Arbre *insertionAVL(Arbre *a, long identifiant, long capacite, long consommation); //Insertion de l'arbre AVL
+Arbre *insertionAVL(Arbre* a, long identifiant, const char* capacite,const char* consommation); //Insertion de l'arbre AVL
 long max(long x, long y); //Fonction pour obtenir la valeur maximale
 Arbre *rotationGauche(Arbre *pivot); //Rotation gauche pour réequilibrer l'arbre
 Arbre *rotationDroite(Arbre *a); //Rotation droite pour réequilibrer l'arbre
 Arbre *doubleRotationGauche(Arbre* a); //Double Rotation Gauche
 Arbre *doubleRotationDroite(Arbre* a); //Double Rotation Droite
 int hauteur(Arbre* a); //Calcul de l'hauteur de l'arbre AVL
-long facteur_equilibre(Arbre* r); //Calcul du facteur d'équilibre
+int facteur_equilibre(Arbre* r); //Calcul du facteur d'équilibre
 void infixe(Arbre* a, FILE* fichier_resultat); //Parcours en ordre croissant (infixe = parcours en ordre) et écriture dans le fichier de sortie
 void freeAVL(Arbre* a); //Libère la mémoire de l'arbre AVL
-void traiterFichier(const char *nomFichier, const char *nomFichierTmp); // traite les fichiers en AVL
+void traiterFichier(const char* nomFichier, const char* nomFichierTmp); // traite les fichiers en AVL
 
 
 #endif //BIBLIO_H
-
 
 
